cf3edcf39e7bb24fa0bbfcb5a468bf0115f92e7c
Tyrant Davis <davistyrant@gmail.com>
2019-10-08 17:01:13 -0500

    Convert App component to a class component.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { Component  } from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App">
-        <nav>
-          <Link to='/'>Landing</Link>
-          <Link to='/library'>Library</Link>
-        </nav>
-        <h1>Block Party Music</h1>
-      </header>
-      <main>
-        <Route exact path="/" component={Landing} />
-        <Route path="/library" component={Library} />
-        <Route path="/album" component={Album} />
-      </main>
-    </div>
-  );
+class App extends Component  {
+  render () {
+    return (
+      <div className="App">
+        <header className="App">
+          <nav>
+            <Link to='/'>Landing</Link>
+            <Link to='/library'>Library</Link>
+          </nav>
+          <h1>Block Party Music</h1>
+        </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
+        </main>
+      </div>
+    );
+  }
 }
-
 export default App;
