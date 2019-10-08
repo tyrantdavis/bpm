07e5df70b23e1316b9aed012df8e74331effe351
Tyrant Davis <davistyrant@gmail.com>
2019-10-08 16:48:34 -0500

    Import Lading, Library, Album components. Resolve missing components error.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
 
 function App() {
   return (
