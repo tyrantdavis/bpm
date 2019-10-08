4c929d64fa2104df006ee282c6e0d59c5d4fbdcf
Tyrant Davis <davistyrant@gmail.com>
2019-10-08 16:28:48 -0500

    Fix compile error.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />
+ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
