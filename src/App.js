93b36a6bad1784f87e7ff238e2aaf7c34747cf8c
Tyrant Davis <davistyrant@gmail.com>
2019-10-08 16:37:26 -0500

    Structure the app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App">
+        <h1>Block Party Music</h1>
       </header>
+      <main>
+        <Route exact path="/" component={Landing} />
+        <Route path="/library" component={Library} />
+        <Route path="/album" component={Album} />
+      </main>
     </div>
   );
 }
