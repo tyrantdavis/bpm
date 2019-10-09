75719949d1b53d736eed85f76319efdabec94398
Tyrant Davis <davistyrant@gmail.com>
2019-10-08 19:04:00 -0500

    Add Link element and import Link from react-router.

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,4 +1,5 @@
-import React, { Component  } from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import albumData from '../data/albums';
 
 class Library extends Component {
@@ -13,12 +14,12 @@ class Library extends Component {
       <section className='library'>
         {
           this.state.albums.map((album, index) =>
-            <div key={index} >
+            <Link to={`/album/${album.slug}`} key={index}>
               <img src={album.albumCover} alt={album.title} />
               <div>{album.title}</div>
               <div>{album.artist}</div>
               <div>{album.songs.length} songs</div>
-            </div>
+            </Link>
           )
         }
       </section>
