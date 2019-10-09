1b701132a4557725064b7d7e51cefc0b53fa7b9c
Tyrant Davis <davistyrant@gmail.com>
2019-10-09 12:44:30 -0500

    Add PlayerBar controls.

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -4,7 +4,28 @@ class PlayerBar extends Component {
   render() { 
     return ( 
       <section className="player-bar">
-        player bar goes here
+        <section id="buttons">
+          <button id="previous">
+            <span className="ion-skip-backward"></span>
+          </button>
+          <button id="play-pause">
+            <span className="ion-play"></span>
+            <span className="ion-pause"></span>
+          </button>
+          <button id="next">
+            <span className="ion-skip-forward"></span>
+          </button>
+        </section>
+        <section id="time-control">
+          <div className="current-time">–:––</div>
+          <input type="range" className="seek-bar" value="0" />
+          <div className="total-time">–:––</div>
+        </section>
+        <section id="volume-control">
+          <div className="icon ion-volume-low"></div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="icon ion-volume-high"></div>
+        </section>
        </section>
      );
   }
