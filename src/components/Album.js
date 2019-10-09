862c8937e593d46b7c64c119b8f08f6814b6f40d
Tyrant Davis <davistyrant@gmail.com>
2019-10-09 13:07:36 -0500

    Find index of current song, calculate new index, call setSong(), play song.

--- src/components/Album.js
+++ src/components/Album.js
@@ -46,6 +46,13 @@ class Album extends Component {
     }
   }
 
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
 
   render() {
     return (
