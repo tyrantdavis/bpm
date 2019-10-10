dd534242f36a52e2140379209927531987518fe9
Tyrant Davis <davistyrant@gmail.com>
2019-10-10 18:47:35 -0500

    Add logo.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component  {
       <div className="App">
         <header className="App">
           <nav>
+            <img src="/assets/images/bpmLogo.png" alt="block party music logo" className="logo"  />
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
           </nav>
