a3c1fc18eb826c6313bb2dcf03c1f631324f2eab
Tyrant Davis <davistyrant@gmail.com>
2019-10-11 11:05:13 -0500

    Add icons to selling-points

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,14 +7,17 @@ const Landing = () => (
     </section>
     <article className="selling-points">
       <section className="point">
+        <span class="ion-music-note"></span>
         <h2 className="point-title">Choose your music</h2>
         <p className="point-description">The world is full of music; why should you have to listen to music that someone else chose?</p>
       </section>
       <section className="point">
+        <span class="ion-radio-waves"></span>
         <h2 className="point-title">Unlimited, streaming, ad-free</h2>
         <p className="point-description">No arbitrary limits. No distractions.</p>
       </section>
       <section className="point">
+        <span class="ion-iphone"></span>
         <h2 className="point-title">Mobile enabled</h2>
         <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
       </section>
