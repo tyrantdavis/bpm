c01a0004882d89deada32e9020debc0316a440fd
Tyrant Davis <davistyrant@gmail.com>
2019-10-15 00:44:38 -0500

    Commence clean up...

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import '../src/styles/normalize.css';
 import '../src/styles/main.css';
 import '../src/styles/landing.css';
