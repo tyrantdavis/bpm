7549f1640eb038022ef39bdbaea0810224f49227
Tyrant Davis <davistyrant@gmail.com>
2019-10-15 10:46:26 -0500

    Udate album data file.

--- src/data/albums.js
+++ src/data/albums.js
@@ -25,16 +25,16 @@ export default [{
     { title: 'Misses Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
   ]
   }, {
-    title: 'The Waters',
-    artist: 'Mac Aroni Toni',
-    releaseInfo: '2009 Geffen Records',
-    albumCover: '/assets/images/album_covers/03.jpg',
-    slug: 'the-waters',
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    releaseInfo: '1909 Spanish Records',
+    albumCover: '/assets/images/album_covers/01.jpg',
+    slug: 'the-colors',
     songs: [
-      { title: 'So Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { title: 'Green With Envy', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { title: 'Red Robin', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { title: 'Pink Panther', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
@@ -50,20 +50,7 @@ export default [{
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
-  }, {
-    title: 'Flash Light',
-    artist: 'Trevor Jet',
-    releaseInfo: '1909 Spanish Records',
-    albumCover: '/assets/images/album_covers/03.jpg',
-    slug: 'the-colors',
-    songs: [
-      { title: 'WTF', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { title: 'Moving On', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { title: 'Hello!', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { title: 'Netflix and Chill', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-      { title: 'We Made It', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-    ]
-  }, {
+  }, , {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
     releaseInfo: '1909 EM',
@@ -102,7 +89,34 @@ export default [{
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
-  }
-];
+  }, {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    releaseInfo: '1909 Spanish Records',
+    albumCover: '/assets/images/album_covers/01.jpg',
+    slug: 'the-colors',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+    ]
+  },
+  // {
+  //   title: 'Flash Light',
+  //   artist: 'Trevor Jet',
+  //   releaseInfo: '1909 Spanish Records',
+  //   albumCover: '/assets/images/album_covers/03.jpg',
+  //   slug: 'the-colors',
+  //   songs: [
+  //     { title: 'WTF', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+  //     { title: 'Moving On', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+  //     { title: 'Hello!', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+  //     { title: 'Netflix and Chill', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+  //     { title: 'We Made It', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+  //   ]
+  // }
+]; 
 
   
