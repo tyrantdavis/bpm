56f811c054f8047515403f28a042665975f0d79a
Tyrant Davis <davistyrant@gmail.com>
2019-10-18 18:46:41 -0500

    Update album.js

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,8 +1,8 @@
 export default [{
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  releaseInfo: '1909 Spanish Records',
-  albumCover: '/assets/images/album_covers/01.jpg',
+  title: 'Keep It Between Us',
+  artist: 'Kelly Rowland',
+  releaseInfo: 'Arista Records',
+  albumCover: '/assets/images/album_covers/krowland.jpeg',
   slug: 'the-colors',
   songs: [
     { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
@@ -28,7 +28,7 @@ export default [{
     title: 'The Colors',
     artist: 'Pablo Picasso',
     releaseInfo: '1909 Spanish Records',
-    albumCover: '/assets/images/album_covers/01.jpg',
+    albumCover: '/assets/images/album_covers/03.jpg',
     slug: 'the-colors',
     songs: [
       { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
@@ -38,10 +38,10 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
+    title: 'The Sound of Silence',
+    artist: 'Mason Jurullo',
     releaseInfo: '1909 EM',
-    albumCover: '/assets/images/album_covers/02.jpg',
+    albumCover: '/assets/images/album_covers/04.jpg',
     slug: 'the-telephone',
     songs: [
       { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
@@ -51,10 +51,36 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, , {
+    title: 'Hello Again',
+    artist: 'Mac Aroni Toni',
+    releaseInfo: '1909 EM',
+    albumCover: '/assets/images/album_covers/05.jpg',
+    slug: 'the-telephone',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+    ]
+  }, {
+    title: 'Stone Free',
+    artist: 'Jim Hendrix',
+    releaseInfo: 'Genius Records',
+    albumCover: '/assets/images/album_covers/jhendrix.jpeg',
+    slug: 'the-colors',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+    ]
+  }, {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
     releaseInfo: '1909 EM',
-    albumCover: '/assets/images/album_covers/02.jpg',
+    albumCover: '/assets/images/album_covers/06.jpeg',
     slug: 'the-telephone',
     songs: [
       { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
@@ -77,46 +103,19 @@ export default [{
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
   }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    releaseInfo: '1909 EM',
-    albumCover: '/assets/images/album_covers/02.jpg',
-    slug: 'the-telephone',
-    songs: [
-      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-    ]
-  }, {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
+    title: 'The Silence of Lonliness',
+    artist: 'Trevor Jet',
     releaseInfo: '1909 Spanish Records',
-    albumCover: '/assets/images/album_covers/01.jpg',
+    albumCover: '/assets/images/album_covers/07.jpeg',
     slug: 'the-colors',
     songs: [
-      { title: 'Blue', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-      { title: 'Green', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-      { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-      { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-      { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
+      { title: 'WTF', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
+      { title: 'Moving On', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
+      { title: 'Hello!', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
+      { title: 'Netflix and Chill', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
+      { title: 'We Made It', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
     ]
-  },
-  // {
-  //   title: 'Flash Light',
-  //   artist: 'Trevor Jet',
-  //   releaseInfo: '1909 Spanish Records',
-  //   albumCover: '/assets/images/album_covers/03.jpg',
-  //   slug: 'the-colors',
-  //   songs: [
-  //     { title: 'WTF', duration: '161.71', audioSrc: '/assets/music/blue.mp3' },
-  //     { title: 'Moving On', duration: '103.96', audioSrc: '/assets/music/green.mp3' },
-  //     { title: 'Hello!', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
-  //     { title: 'Netflix and Chill', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
-  //     { title: 'We Made It', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-  //   ]
-  // }
+  }
 ]; 
 
   
