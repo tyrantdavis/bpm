5ab7b8e94799cd400a8127a9e1887d41c1d4cc79
Tyrant Davis <davistyrant@gmail.com>
2019-10-20 14:42:14 -0500

    Update hero and logo urls. Remove unused asstes.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
-import Logo from './sampleBpmLogo.png';
+import Logo from './logo/bpmLogo.png';
 class App extends Component  {
   render () {
     return (
