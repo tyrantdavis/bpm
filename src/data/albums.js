638f2a7f21b77bb69a8a33829c2231145723305f
Tyrant Davis <davistyrant@gmail.com>
2019-10-20 13:55:19 -0500

    Update album data.

--- src/data/albums.js
+++ src/data/albums.js
@@ -11,6 +11,169 @@ export default [{
     { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
     { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
   ]
+},{
+    title: 'Hello',
+    artist: 'Mac Minny',
+    releaseInfo: 'Geffen Records',
+    albumCover: '/images/album_covers/03.jpg',
+    slug: 'say-hello',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' },
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' }
+
+    ]
+  }, {
+    title: 'Stone Free',
+    artist: 'Jimmy Hendrix',
+    releaseInfo: 'RCA Records',
+    albumCover: '/images/album_covers/jhendrix.jpeg',
+    slug: 'stone-free',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+    title: 'The Silence',
+    artist: 'Mason Jos',
+    releaseInfo: 'Arista Records',
+    albumCover: '/images/album_covers/04.jpg',
+    slug: 'the-silence',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+    title: 'Were Do We Go',
+    artist: 'Mac Aroni Toni',
+    releaseInfo: 'RCA Records',
+    albumCover: '/images/album_covers/05.jpg',
+    slug: 'we-go',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  releaseInfo: '1909 EM',
+  albumCover: '/images/album_covers/02.jpg',
+  slug: 'the-telephone',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+  ]
+  }, {
+    title: 'Hello',
+    artist: 'Mac Minny',
+    releaseInfo: 'Geffen Records',
+    albumCover: '/images/album_covers/03.jpg',
+    slug: 'say-hello',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' },
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' }
+
+    ]
+  },{
+  title: 'Keep It Between Us Luv',
+  artist: 'Kelly Rowland',
+  releaseInfo: 'Arista Records',
+  albumCover: '/images/album_covers/krowland.jpeg',
+  slug: 'the-colors',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+  ]
+},{
+    title: 'Stone Free',
+    artist: 'Jimmy Hendrix',
+    releaseInfo: 'RCA Records',
+    albumCover: '/images/album_covers/jhendrix.jpeg',
+    slug: 'stone-free',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+    title: 'The Silence',
+    artist: 'Mason Jos',
+    releaseInfo: 'Arista Records',
+    albumCover: '/images/album_covers/04.jpg',
+    slug: 'the-silence',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+    title: 'Were Do We Go',
+    artist: 'Mac Aroni Toni',
+    releaseInfo: 'RCA Records',
+    albumCover: '/images/album_covers/05.jpg',
+    slug: 'we-go',
+    songs: [
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
+    ]
+  }, {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  releaseInfo: '1909 EM',
+  albumCover: '/images/album_covers/02.jpg',
+  slug: 'the-telephone',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+  ]
+  }, {
+    title: 'Hello',
+    artist: 'Mac Minny',
+    releaseInfo: 'Geffen Records',
+    albumCover: '/images/album_covers/03.jpg',
+    slug: 'say-hello',
+    songs: [
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' },
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' }
+
+    ]
+  },{
+  title: 'Keep It Between Us Luv',
+  artist: 'Kelly Rowland',
+  releaseInfo: 'Arista Records',
+  albumCover: '/images/album_covers/krowland.jpeg',
+  slug: 'the-colors',
+  songs: [
+    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+  ]
 }, {
   title: 'The Telephone',
   artist: 'Guglielmo Marconi',
@@ -25,95 +188,30 @@ export default [{
     { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
   ]
   }, {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    releaseInfo: '1909 Spanish Records',
+    title: 'Hello',
+    artist: 'Mac Minny',
+    releaseInfo: 'Geffen Records',
     albumCover: '/images/album_covers/03.jpg',
-    slug: 'the-colors',
+    slug: 'say-hello',
     songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
-    ]
-  }, {
-    title: 'The Sound of Silence',
-    artist: 'Mason Jurullo',
-    releaseInfo: '1909 EM',
-    albumCover: '/images/album_covers/04.jpg',
-    slug: 'the-telephone',
-    songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
-    ]
-  }, , {
-    title: 'Hello Again',
-    artist: 'Mac Aroni Toni',
-    releaseInfo: '1909 EM',
-    albumCover: '/images/album_covers/05.jpg',
-    slug: 'the-telephone',
-    songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' },
+      { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' }
+
     ]
   }, {
     title: 'Stone Free',
-    artist: 'Jim Hendrix',
-    releaseInfo: 'Genius Records',
+    artist: 'Jimmy Hendrix',
+    releaseInfo: 'RCA Records',
     albumCover: '/images/album_covers/jhendrix.jpeg',
-    slug: 'the-colors',
+    slug: 'stone-free',
     songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
-    ]
-  }, {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    releaseInfo: '1909 EM',
-    albumCover: '/images/album_covers/06.jpeg',
-    slug: 'the-telephone',
-    songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
-    ]
-  }, {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    releaseInfo: '1909 Spanish Records',
-    albumCover: '/images/album_covers/01.jpg',
-    slug: 'the-colors',
-    songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
-    ]
-  }, {
-    title: 'The Silence of Lonliness',
-    artist: 'Trevor Jet',
-    releaseInfo: '1909 Spanish Records',
-    albumCover: '/images/album_covers/07.jpeg',
-    slug: 'the-colors',
-    songs: [
-    { title: 'Blue', duration: '161.71', audioSrc: '/music/blue.mp3' },
-    { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
-    { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' },
-    { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
-    { title: 'Magenta', duration: '374.22', audioSrc: '/music/magenta.mp3' }
+      { title: 'Pink', duration: '153.14', audioSrc: '/music/pink.mp3' },
+      { title: 'Green', duration: '103.96', audioSrc: '/music/green.mp3' },
+      { title: 'Red', duration: '268.45', audioSrc: '/music/red.mp3' }
     ]
   }
 ]; 
