522c4f1e6484ce2bf2dc88ace74c9dbc46c246cd
Tyrant Davis <davistyrant@gmail.com>
2019-10-21 11:31:02 -0500

    Reposition logo.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 import Logo from './logo/bpmLogo.png';
+
 class App extends Component  {
   render () {
     return (
